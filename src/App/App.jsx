89da2544d3fc328999b1adc24e921e54830535b9
champix56 <desorbaix@free.fr>
2023-09-11 17:30:45 +0100

    gestion etat et effects sur function

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,20 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from './components/Button/Button.jsx';
 const App = (props) => {
 
-  const [state, setState] = useState({counter:0,hello:"world"})
+  const [state, setState] = useState({ counter: -1, hello: "world" })
+  useEffect(() => {
+    console.log('dans le useEffect', state);
 
-  
+    // return () => {
+    //   second
+    // }
+  }, [state])
+  useEffect(() => {
+    console.log('montage');
+    setState({ ...state, counter: 0 })
+  }, [])
+
   return <div className='App'>
     valeur de counter : {state.counter}<hr />
     <Button bgColor="tomato" onClick={() => {
-      setState({...state,counter:state.counter-1});
-      if(state.counter===0)alert('on est a zero')
+      setState({ ...state, counter: state.counter - 1 });
+      if (state.counter === 0) alert('on est a zero')
       console.log(state);
     }} >-</Button>
     <Button
       onClick={() => {
-        setState({...state,counter:state.counter+1});
+        setState({ ...state, counter: state.counter + 1 });
         console.log(state);
 
       }} >+</Button>
--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
-import style from './Button.module.css'
-import PropTypes from 'prop-types'
-
+import React, { useEffect, useState } from "react";
+import style from "./Button.module.css";
+import PropTypes from "prop-types";
 
 const Button = (props) => {
-    //console.log(props);
-    return <button onClick={(props.onClick)} className={style.Button} style={{
+  const [isClicked, setIsClicked] = useState(false);
+  //console.log(props);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 180);
+  }, [isClicked]);
+
+  return (
+    <button
+      onClick={(evt) => {
+        setIsClicked(true);
+        props.onClick();
+      }}
+      className={isClicked ? style.Button + " " + style.clicked : style.Button}
+      style={{
         backgroundColor: props.bgColor,
         ...props.style,
-    }}>{props.children}</button>
-}
+      }}
+    >
+      {props.children}
+    </button>
+  );
+};
 
-Button.propTypes={
-    children: PropTypes.any.isRequired,
-    bgColor: PropTypes.oneOf(['skyblue','tomato','transparent']),
-    onClick: PropTypes.func.isRequired,
-    style: PropTypes.shape({
-        width: PropTypes.string,
-        padding: PropTypes.string,
-    })
-}
-Button.defaultProps={
-    bgColor:'skyblue'
-}
+Button.propTypes = {
+  children: PropTypes.any.isRequired,
+  bgColor: PropTypes.oneOf(["skyblue", "tomato", "transparent"]),
+  onClick: PropTypes.func.isRequired,
+  style: PropTypes.shape({
+    width: PropTypes.string,
+    padding: PropTypes.string,
+  }),
+};
+Button.defaultProps = {
+  bgColor: "skyblue",
+};
 
-export default Button
+export default Button;
