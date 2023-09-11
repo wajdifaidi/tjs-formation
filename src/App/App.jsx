3fb2b90f33940167d24acd8f2d44f338c9d4ba26
champix56 <desorbaix@free.fr>
2023-09-11 16:01:54 +0100

    props.children

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,7 +3,10 @@ import Button from './components/Button/Button.jsx';
 const App = (props) => {
   return <div className='App'>
     DEMAT BREIZH<hr />
-    <Button text="Bejamin" color="red" style={{ width: '254px', padding: '60px 80px' }} />
+    <Button  color="red" style={{ width: '254px', padding: '60px 80px' }} >
+        Benjamin 
+        <img src="https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/tick_success_done_valid-2-256.png" style={{height:"32px",width:"32px"}}/>
+    </Button>
     <Button
       text="button"
       bgColor='skyblue'
--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -4,14 +4,15 @@ import PropTypes from 'prop-types'
 
 
 const Button = (props) => {
+    console.log(props);
     return <button onClick={(props.onClick)} className={style.Button} style={{
         backgroundColor: props.bgColor,
         ...props.style,
-    }}>{props.text}</button>
+    }}>{props.children}</button>
 }
 
 Button.propTypes={
-    text: PropTypes.string.isRequired,
+    children: PropTypes.any.isRequired,
     bgColor: PropTypes.oneOf(['skyblue','tomato','transparent']),
     onClick: PropTypes.func.isRequired,
     style: PropTypes.shape({
