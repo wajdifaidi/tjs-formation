264419477cec90b2b76f62b9657dc7e696f05144
champix56 <desorbaix@free.fr>
2023-09-12 11:44:46 +0100

    ajout navbar react-bootstrap

--- src/App/components/ui/Navbar/Navbar.jsx
+++ src/App/components/ui/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Navbar.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Navbar.module.css";
+import { Container, Nav, Navbar as NavBar } from "react-bootstrap";
+import '/node_modules/bootstrap/dist/css/bootstrap.css'
+import '/node_modules/jquery/dist/jquery.js'
+import '/node_modules/bootstrap/dist/js/bootstrap.js'
 
 const Navbar = () => (
   <div className={styles.Navbar} data-testid="Navbar">
-    Navbar Component
+    <NavBar bg="primary" data-bs-theme="dark">
+      <Container>
+        <NavBar.Brand href="#home">Navbar</NavBar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </NavBar>
   </div>
 );
 
