b175bb1fc0917ec50afc559b12b6488a031a05a6
champix56 <desorbaix@free.fr>
2023-09-12 13:49:36 +0100

    composant pour le svg viewer

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import FlexW1G from "./components/layout/FlexW1G/FlexW1G";
 import FlexH3G from "./components/layout/FlexH3G/FlexH3G";
 import MemeForm from "./components/MemeForm/MemeForm";
 import Footer from "./components/ui/Footer/Footer";
 import Navbar from "./components/ui/Navbar/Navbar";
 import Header from "./components/ui/Header/Header";
-
+import MemeViewer from "./components/ui/MemeViewer/MemeViewer";
+import { DummyMeme } from "./interfaces/common";
 const App = () => {
+  const [current, setcurrent] = useState(DummyMeme);
+  const [images, setimages] = useState([
+    {
+      id: 0,
+      url: "https://risibank.fr/cache/medias/0/12/1285/128588/full.png",
+      w: 467,
+      h: 348,
+    },
+  ]);
   return (
     <div className="App">
       <FlexH3G>
         <Header></Header>
         <Navbar></Navbar>
         <FlexW1G>
-          <div>Viewer</div>
+          <MemeViewer
+            meme={current}
+            image={images.find((img) => img.id === current.imageId)}
+            basePath=""
+          ></MemeViewer>
           <MemeForm></MemeForm>
         </FlexW1G>
         <Footer></Footer>
