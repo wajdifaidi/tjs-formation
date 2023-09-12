0240af8e77ed299da50d5c7486485b5c11f8be91
champix56 <desorbaix@free.fr>
2023-09-12 11:25:29 +0100

    construction des composant ui pur sans props

--- src/App/components/ui/Footer/Footer.jsx
+++ src/App/components/ui/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css';
 
 const Footer = () => (
   <div className={styles.Footer} data-testid="Footer">
-    Footer Component
+   Copyright DESORBAIX &copy; 2023
   </div>
 );
 
--- src/App/components/ui/Header/Header.jsx
+++ src/App/components/ui/Header/Header.jsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 
 const Header = () => (
   <div className={styles.Header} data-testid="Header">
-    Header Component
+    <span className={styles.meme}>Meme</span>.<span className={styles.js}>js</span>
   </div>
 );
 
