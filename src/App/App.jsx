707e05909a0f04d3ec167363873f2d3c4aa02cb0
wajdi faidi <wajdi.faidi@gmail.com>
2023-09-13 15:33:10 +0100

    router

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,25 +1,44 @@
 import React, { useEffect, useState } from "react";
 import FlexW1G from "./components/layout/FlexW1G/FlexW1G";
 import FlexH3G from "./components/layout/FlexH3G/FlexH3G";
-import {MemeFormStoredData} from "./components/MemeForm/MemeForm";
+import { MemeFormStoredData } from "./components/MemeForm/MemeForm";
 import Footer from "./components/ui/Footer/Footer";
 import Navbar from "./components/ui/Navbar/Navbar";
 import Header from "./components/ui/Header/Header";
 import { MemeViewerStoredCurrent } from "./components/ui/MemeViewer/MemeViewer";
-import MemeThumbnail, { MemeThumbnailStoredDatas } from "./components/MemeThumbnail/MemeThumbnail";
+import { MemeThumbnailStoredDatas } from "./components/MemeThumbnail/MemeThumbnail";
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
     <div className="App">
       <FlexH3G>
-        <Header></Header>
-        <Navbar></Navbar>
-        <FlexW1G>
-          <MemeViewerStoredCurrent 
-            basePath=""/>
-          <MemeFormStoredData/>
-        </FlexW1G>
-        <MemeThumbnailStoredDatas/>
-        <Footer></Footer>
+        <Header />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Bienvenue sur la homepage</h1>} />
+          <Route path="/thumbnail" element={<MemeThumbnailStoredDatas />} />
+          <Route 
+            path="/editor"
+            element={
+              <FlexW1G>
+                <MemeViewerStoredCurrent basePath="" />
+                <MemeFormStoredData />
+              </FlexW1G>
+            }
+          />
+           <Route
+            path="/editor/:id"
+            element={
+              <FlexW1G>
+                <MemeViewerStoredCurrent basePath="" />
+                <MemeFormStoredData />
+              </FlexW1G>
+            }
+          />
+        </Routes>
+
+        <Footer />
       </FlexH3G>
     </div>
   );
--- src/App/components/ui/Navbar/Navbar.jsx
+++ src/App/components/ui/Navbar/Navbar.jsx
@@ -5,20 +5,32 @@ import { Container, Nav, Navbar as NavBar } from "react-bootstrap";
 import '/node_modules/bootstrap/dist/css/bootstrap.css'
 import '/node_modules/jquery/dist/jquery.js'
 import '/node_modules/bootstrap/dist/js/bootstrap.js'
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Navbar = () => (
+  <>
   <div className={styles.Navbar} data-testid="Navbar">
     <NavBar bg="primary" data-bs-theme="dark">
       <Container>
-        <NavBar.Brand href="#home">Navbar</NavBar.Brand>
+        <LinkContainer to="/"><NavBar.Brand>Navbar</NavBar.Brand></LinkContainer>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
+        <LinkContainer to="/thumbnail"><Nav.Link >Thumbnail</Nav.Link></LinkContainer>
+         <LinkContainer to="/editor"><Nav.Link >Pricing</Nav.Link></LinkContainer>
         </Nav>
       </Container>
     </NavBar>
   </div>
+  <hr/>
+  <div>
+    <Link to="/">Home</Link>&nbsp;
+    <Link to="/thumbnail">thumbnail</Link>&nbsp;
+    <Link to="/editor">New</Link>&nbsp;
+    <Link to="/editor/1">Edit id 0</Link>
+  </div>
+  <hr/>
+  </>
 );
 
 Navbar.propTypes = {};
